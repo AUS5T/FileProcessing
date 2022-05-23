163af7625a4fcad133fecdd952b5a7f8987b3c0f
AUS5T <arm1996@comcast.net>
2022-05-22 19:15:06 -0700

    updated

--- FileProcessingProgram.py
+++ FileProcessingProgram.py
@@ -15,19 +15,18 @@ import logging #importing logging
 
 # CRITICAL: A serious error, indicating that the program itself may be unable to continue running.
 
-logging.basicConfig(filename='test.log', level=logging.DEBUG, format='%(asctime)s:')
 
 D1 = input("Directory path to save file in:") #Asking for directory
 
 while True:
     if not os.path.exists(D1):
-        print("some error message")
+        print("Path does not exist")
         continue
     if not os.path.isdir(D1):
-        print("another error message")
+        print("Path does not exist")
         continue
     break
-print("Directory exists!")
+print("Path exists!")
 
 file_name = input("Enter what you would like the file to be named:")
 
@@ -38,7 +37,8 @@ address = input("Enter Your address:")
 phone = input("Enter your phone number:")
 
 x = name, address, phone
-y = open(D1, 'w')
-y.write(x)
 
-open(D1)
+open(D1, 'w+')
+y = open(file_name, 'w+')
+y.write(str(x))
+
